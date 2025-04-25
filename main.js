1e7b2adde3084fda885b4dba35d9171225f22e81
Liriannys19 <141455405+liriannys2007@users.noreply.github.com>
2025-04-25 11:53:28 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-const botoes = document.querySelectorAll(".botao")
-const textos = document.querySelectorAll("aba-conteudo")
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll("aba-conteudo");
+const contadores = document.querySelectorAll(".contador");
+
+const tempoObjetivo01 = new Date("2025-05-05T00:00:00");
+let tempoAtual = new Date();
+
+contadores[0].textContent =tempoObjetivo01 - tempoAtual
 
 for(let i=0; i<botoes.length; i++){
     
@@ -10,8 +16,22 @@ for(let i=0; i<botoes.length; i++){
             textos[j].classList.remove('ativo');
         }
        
-        botoes[i].classList.add('ativo')
-        textos[i].classList.add('ativo')
-
+        botoes[i].classList.add('ativo');
+        textos[i].classList.add('ativo');
+  
     }
+}
+
+function claculatempo(tempoObjetivo){
+    let tempoAtual();
+    let tempofinal = tempoObjetivo - tempoAtual
+
+    let segundos = match.floor(tempofinal/1000);
+    let minutos = match.floor(segundos/60);
+    let horas = match.floor(minutos/60);
+    let dias = match.floor(horas/24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
 }
